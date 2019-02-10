f00eb0508c25604c23aae66be7f8e8f71863c845
Daniel <danielpires6@hotmail.com>
2019-02-10 14:37:17 -0200

    Criacao do do projeto e configurações iniciais

---
+++ ListaDeSupermercado/ListaDeSupermercado/ListaDeSupermercado/Banco/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ListaDeSupermercado.Banco
+{
+    class Database
+    {
+        private SQLiteConnection _conexao;
+
+        public Database()
+        {
+            _conexao = new SQLiteConnection();
+        }
+    }
+}
---
+++ ListaDeSupermercado/ListaDeSupermercado/ListaDeSupermercado/Usuarios/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaDeSupermercado.Usuarios
+{
+    class Usuario
+    {
+        private string Nome { get; set; }
+        private string Sobrenome { get; set; }
+        private string Login { get; set; }
+        private string Senha { get; set; }
+    }
+}
